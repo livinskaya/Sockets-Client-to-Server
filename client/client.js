adb11aa78f4e833283886f62f3f76e0e2c367c98
Lando Winzen <winzenlando@gmail.com>
2025-11-12 15:29:03 +0100

    02b -  Exercise 1 - 4 finished

--- client/client.js
+++ client/client.js
@@ -1,18 +1,35 @@
 const net = require("net");
+const readline = require("node:readline");
 
+const username = process.argv[2] || "Unbekannt";
 const host = "127.0.0.1";
 const port = 4000;
 
 const client = new net.Socket();
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 client.connect(port, host, () => {
   console.log("Conncted");
-  client.write("From Client: Hellllooooooo_test");
+  client.write(`user: ${username} hat sich verbunden`);
 });
 
 client.on("data", (data) => {
   console.log(data.toString());
-  client.end();
+  rl.prompt(true);
+});
+
+rl.on("line", (input) => {
+  if (input.toLocaleLowerCase() === "/exit") {
+    console.log("Verbindung geschlossen");
+    client.end;
+    rl.close();
+  } else {
+    client.write(`[${username}]: ${input}`);
+  }
 });
 
 client.on("close", () => {
--- server/server.js
+++ server/server.js
@@ -3,16 +3,29 @@ const net = require("net");
 const host = "127.0.0.1";
 const port = 4000;
 
+const clients = [];
+
 const server = net.createServer((socket) => {
-  console.log("Connected");
+  socket.write("Welcome to the Server!");
+
+  clients.push(socket);
 
   socket.on("data", (data) => {
-    console.log(data.toString());
-    socket.write(data.toString());
+    const message = data.toString().trim();
+    console.log("Recieved Message");
+
+    for (const client of clients) {
+      if (client !== socket) {
+        client.write(message);
+      }
+    }
+    console.log(message);
   });
 
   socket.on("end", () => {
     console.log("Disconnected");
+    const index = clients.indexOf(socket);
+    if (index !== 1) clients.splice(index, 1);
   });
 
   socket.on("error", (err) => {
