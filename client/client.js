3be5f8e602e7a5517a4cc68b1d6a287af5799246
Lando Winzen <winzenlando@gmail.com>
2025-11-12 10:20:13 +0100

    02a - Exercise 2 finished.

--- client/client.js
+++ client/client.js
@@ -1,14 +1,16 @@
 const net = require("net");
 
 const host = "127.0.0.1";
-const port = 80;
+const port = 4000;
 
 const client = new net.Socket();
 
 client.connect(port, host, () => {
   console.log("Conncted");
 
-  const request = "GET / HTTP/1.1\r\n" + `Host: ${host}\r\n`;
+  const request = `GET / HTTP/1.1\r\nHost: ${host}\r\nConnection: close\r\n\r\n`;
+
+  client.write(request);
 });
 
 client.on("data", (data) => {
